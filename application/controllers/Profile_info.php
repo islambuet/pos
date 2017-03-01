a578f5d94bd129abb524f1c936697091fc1a76ae
islambuet <islambuet@gmail.com>
2017-03-01 20:32:23 +0600

    Profile tasks completed

---
+++ application/controllers/Profile_info.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Profile_info extends Root_Controller
+{
+    private  $message;
+    public $permissions;
+    public $controller_url;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->message="";
+        $this->permissions=User_helper::get_permission('Profile_info');
+        $this->controller_url='profile_info';
+    }
+    public function index($action="details",$id=0)
+    {
+        if($action=="details")
+        {
+            $this->system_details();
+        }
+        else
+        {
+            $this->system_details();
+        }
+    }
+
+    private function system_details()
+    {
+        if(isset($this->permissions['action0']) && ($this->permissions['action0']==1))
+        {
+            $user=User_helper::get_user();
+            $user_id=$user->user_id;
+            $this->db->from($this->config->item('table_pos_setup_user').' user');
+            $this->db->select('user.user_name,user.status');
+            $this->db->select('user_info.user_id,user_info.name,user_info.ordering,user_info.date_birth,user_info.gender,user_info.status_marital,user_info.blood_group,user_info.mobile_no,user_info.nid,user_info.address');
+            $this->db->select('ug.name group_name');
+            $this->db->select('designation.name designation_name');
+            $this->db->join($this->config->item('table_pos_setup_user_info').' user_info','user.id = user_info.user_id','INNER');
+            $this->db->join($this->config->item('table_system_user_group').' ug','ug.id = user_info.user_group','LEFT');
+            $this->db->join($this->config->item('table_pos_setup_designation').' designation','designation.id = user_info.designation','LEFT');
+            $this->db->where('user_info.revision',1);
+            $this->db->where('user.id',$user_id);
+
+            $data['user_info']=$this->db->get()->row_array();
+            $data['title']=$data['user_info']['name'];
+
+            $ajax['status']=true;
+            $ajax['system_content'][]=array("id"=>"#system_content","html"=>$this->load->view($this->controller_url."/details",$data,true));
+            if($this->message)
+            {
+                $ajax['system_message']=$this->message;
+            }
+            $ajax['system_page_url']=site_url($this->controller_url.'/index/details/'.$user_id);
+            $this->json_return($ajax);
+        }
+        else
+        {
+            $ajax['status']=false;
+            $ajax['system_message']=$this->lang->line("YOU_DONT_HAVE_ACCESS");
+            $this->json_return($ajax);
+        }
+    }
+
+}
---
+++ application/views/profile_info/details.php
@@ -0,0 +1,119 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$CI=& get_instance();
+?>
+<div class="row widget">
+    <div class="widget-header">
+        <div class="title">
+            <?php echo $title; ?>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_USERNAME');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['user_name'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_NAME');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['name'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_USER_GROUP');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['group_name'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_DESIGNATION_NAME');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['designation_name'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_DATE_BIRTH');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo System_helper::display_date($user_info['date_birth']);?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_GENDER');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['gender'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_MARITAL_STATUS');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['status_marital'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_NID');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['nid'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_ADDRESS');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['address'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_BLOOD_GROUP');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['blood_group'];?></label>
+        </div>
+    </div>
+    <div class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_MOBILE_NO');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['mobile_no'];?></label>
+        </div>
+    </div>
+    <div style="" class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('LABEL_ORDER');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['ordering'];?></label>
+        </div>
+    </div>
+    <div style="" class="row show-grid">
+        <div class="col-xs-4">
+            <label class="control-label pull-right"><?php echo $CI->lang->line('STATUS');?></label>
+        </div>
+        <div class="col-sm-4 col-xs-8">
+            <label class="control-label"><?php echo $user_info['status'];?></label>
+        </div>
+    </div>
+
+</div>
+
+<div class="clearfix"></div>
